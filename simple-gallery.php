6c7fbf8aa7ca5256b2a6f4232b06d042fc5eab9b
janbthomas <janbthomas@gmail.com>
2014-03-14 22:28:53 -0700

    Create simple-gallery.php

---
+++ simple-gallery.php
@@ -0,0 +1,10 @@
+<div class="gallery">
+		<a tabindex="1"><img src="gallery/images/londonparis_029.jpg"></a>
+		<a tabindex="1"><img src="gallery/images/londonparis_087.jpg"></a>
+		<a tabindex="1"><img src="gallery/images/londonparis_093.jpg"></a>
+	
+		<a tabindex="1"><img src="gallery/images/londonparis_128.jpg"></a>
+		<a tabindex="1"><img src="gallery/images/londonparis_150.jpg"></a>
+		<a tabindex="1"><img src="gallery/images/londonparis_201.jpg"></a>
+	</div>
+
